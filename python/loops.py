23145cf0dea71d91db88cc566dc4b9caf37ce0dc
JB <jb@unite.systems>
2020-07-01 21:38:22 +0000

    added read wrtie

--- python/loops.py
+++ python/loops.py
@@ -1,1 +1,17 @@
-import json
+
+planes = ["P40", "P51", "P38"]
+
+# Iterate through Loop
+n = 0
+print("--Standard For Loop--")
+for line in planes:
+    print("List line number: " + str(n) + " Plane: " + line)
+    n = n + 1
+
+# Direct Call of List items
+print("--Direct Call in List--")
+print("List line 0: " + planes[0] + " | List item 1: " + planes[1] + " | List item 2:  " + planes[2])
+
+# Python One line Loops
+print("--One line loop iteration--")
+[ print(p) for p in planes ]
