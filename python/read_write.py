d988e0dd0180f433b1b51f0b95dba6bb12617b91
JB <jb@unite.systems>
2020-07-01 21:54:59 +0000

    added json examples.

--- python/read_write.py
+++ python/read_write.py
@@ -1,2 +1,11 @@
 import json
 
+py_dictionary = {}
+
+# Write Dict to Json
+with open('file.json', 'w') as f:
+    json.dump(py_dictionary, f, indent=4, sort_keys=True)
+
+# Load Json into dict
+with open('file.json') as f:
+        py_dict = json.load(f)
